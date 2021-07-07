34df3fd121dbc677fcb2e0f7b719173c775d130e
김정태(Jeongtae Kim)/App개발팀/11ST <origogi.kim@sk.com>
2021-07-07 16:55:23 +0900

    move first item in list when touch instagram icon

--- app/src/main/java/com/origogi/instgramclone/ui/view/InstagramHome.kt
+++ app/src/main/java/com/origogi/instgramclone/ui/view/InstagramHome.kt
@@ -3,15 +3,19 @@ package com.origogi.instgramclone.ui.view
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddBox
 
 import androidx.compose.material.icons.filled.Send
+import androidx.compose.material.icons.outlined.AddBox
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -20,13 +24,16 @@ import androidx.compose.ui.unit.dp
 import com.origogi.instgramclone.R
 import com.origogi.instgramclone.data.DataDummy
 import com.origogi.instgramclone.ui.theme.InstgramcloneTheme
+import kotlinx.coroutines.launch
 
 @Composable
 fun InstagramHome() {
     InstgramcloneTheme {
 
-        Scaffold(topBar = { Appbar() }) {
-            InstagramHomeContents()
+        val listState = rememberLazyListState()
+
+        Scaffold(topBar = { Appbar(listState) }) {
+            InstagramHomeContents(listState)
         }
     }
 
@@ -35,12 +42,15 @@ fun InstagramHome() {
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun Appbar() {
+fun Appbar(listState: LazyListState) {
+    val coroutineScope = rememberCoroutineScope()
+
+
     TopAppBar(
         navigationIcon = null,
         actions = {
             IconButton(onClick = { /*TODO*/ }) {
-                Icon(Icons.Default.AddBox, contentDescription = "")
+                Icon(Icons.Outlined.AddBox, contentDescription = "")
             }
             IconButton(onClick = { /*TODO*/ }) {
                 Icon(Icons.Outlined.FavoriteBorder, contentDescription = "")
@@ -51,16 +61,15 @@ fun Appbar() {
         },
         title = {
             Surface(onClick = {
-                //TODO : Move first item on List View
+                coroutineScope.launch {
+                    listState.animateScrollToItem(index = 0)
+                }
             }) {
                 Image(
                     painter = painterResource(id = R.drawable.instagram_logo),
                     contentDescription = "",
                 )
-
             }
-
-
         },
 
         backgroundColor = MaterialTheme.colors.background,
@@ -72,20 +81,21 @@ fun Appbar() {
 }
 
 @Composable
-fun InstagramHomeContents() {
+fun InstagramHomeContents(listState: LazyListState) {
     Column {
         InstagramStories()
         Divider()
-        InstagramPostList()
-        
+        InstagramPostList(listState)
+
     }
 }
 
 @Composable
-fun InstagramPostList() {
+fun InstagramPostList(listState: LazyListState) {
     val posts = DataDummy.storyList.filter { it.storyImageId != 0 }
-    LazyColumn {
-        items( items = posts) { post ->
+
+    LazyColumn(state = listState) {
+        items(items = posts) { post ->
             InstagramListItem(post = post)
         }
     }
