42c138f2b37a01ee6472d1379e584cf913064764
김정태(Jeongtae Kim)/App개발팀/11ST <origogi.kim@sk.com>
2021-07-18 20:41:42 +0900

    remove unused package

--- app/src/main/java/com/origogi/instgramclone/ui/view/InstagramHome.kt
+++ app/src/main/java/com/origogi/instgramclone/ui/view/InstagramHome.kt
@@ -1,6 +1,5 @@
 package com.origogi.instgramclone.ui.view
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -11,17 +10,10 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AddBox
-
-import androidx.compose.material.icons.filled.Send
-import androidx.compose.material.icons.outlined.AddBox
-import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -42,8 +34,6 @@ fun InstagramHome() {
             InstagramHomeContents(listState)
         }
     }
-
-
 }
 
 @OptIn(ExperimentalMaterialApi::class)
