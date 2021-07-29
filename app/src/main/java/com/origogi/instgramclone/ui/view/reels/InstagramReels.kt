d853366e7093c1a4e5647aacc70e25092c61e21a
origogi <rlawjdxo1212@naver.com>
2021-07-29 18:47:27 +0900

    add reels appbar

--- app/src/main/java/com/origogi/instgramclone/ui/view/reels/InstagramReels.kt
+++ app/src/main/java/com/origogi/instgramclone/ui/view/reels/InstagramReels.kt
@@ -1,19 +1,55 @@
 package com.origogi.instgramclone.ui.view.reels
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.origogi.instgramclone.R
+import com.origogi.instgramclone.ui.theme.InstgramcloneTheme
 
 @Composable
-fun InstagramReels() {
-    Column() {
-        Text(text = "Reels")
+fun InstagramReels(modifier: Modifier = Modifier) {
+
+    Surface {
+        Row {
+            ReelsTopbar(modifier)
+        }
+    }
+
+}
+
+@Composable
+fun ReelsTopbar(modifier: Modifier = Modifier) {
+    Row(
+        modifier
+            .fillMaxWidth()
+            .padding(15.dp), horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically) {
+        Text(text = "Reels", style = MaterialTheme.typography.h5.copy(fontWeight = FontWeight.Bold))
+        Icon(painter = painterResource(id = R.drawable.ic_outlined_camera), contentDescription = "")
     }
 }
 
 @Composable
-@Preview
+fun VerticalButton() {
+    Column {
+
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
 fun InstagramReelsPreview() {
-    InstagramReels()
+
+    InstgramcloneTheme(darkTheme = true) {
+        InstagramReels()
+
+    }
 }
