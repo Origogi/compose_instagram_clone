30ab2f8ac4e756154c4b3d620b6e0ad920d3b3d8
origogi <rlawjdxo1212@naver.com>
2021-08-22 21:29:14 +0900

    update stagged itme

--- app/src/main/java/com/origogi/instgramclone/ui/view/search/Instagramsearch.kt
+++ app/src/main/java/com/origogi/instgramclone/ui/view/search/Instagramsearch.kt
@@ -1,8 +1,13 @@
 package com.origogi.instgramclone.ui.view.search
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -14,16 +19,116 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.solver.widgets.Rectangle
+import com.origogi.instgramclone.R
 import com.origogi.instgramclone.ui.const.icon
 
 @Composable
 fun InstagramSearch() {
 
-    Column() {
+    val scrollState = rememberScrollState()
+
+
+    Column(modifier = Modifier.verticalScroll(scrollState)) {
         SearchAppbar()
+        StaggeredItem1()
+        StaggeredItem2()
+        StaggeredItem3()
+        StaggeredItem4()
     }
+}
+
+@Composable
+fun StaggeredItem1() {
+    Column() {
+        Row() {
+            SquareImage(id = R.drawable.s1, modifier = Modifier.weight(1f))
+            SquareImage(id = R.drawable.s1, modifier = Modifier.weight(1f))
+            SquareImage(id = R.drawable.s1, modifier = Modifier.weight(1f))
+        }
+        Row() {
+            SquareImage(id = R.drawable.s1, modifier = Modifier.weight(1f))
+            SquareImage(id = R.drawable.s1, modifier = Modifier.weight(1f))
+            SquareImage(id = R.drawable.s1, modifier = Modifier.weight(1f))
+        }
+    }
+}
+
+@Composable
+fun StaggeredItem2() {
+    Row() {
+        SquareImage(id = R.drawable.s1, modifier = Modifier.weight(2f))
+        Column(Modifier.weight(1f)) {
+            SquareImage(id = R.drawable.s1, modifier = Modifier)
+            SquareImage(id = R.drawable.s1, modifier = Modifier)
+        }
+    }
+}
+
+@Composable
+fun StaggeredItem3() {
+    Row() {
+        RectangleImage(id = R.drawable.s1, modifier = Modifier.weight(1f))
+        Column(Modifier.weight(2f)) {
+            Row() {
+                SquareImage(id = R.drawable.s1, modifier = Modifier.weight(1f))
+                SquareImage(id = R.drawable.s1, modifier = Modifier.weight(1f))
+
+            }
+            Row() {
+                SquareImage(id = R.drawable.s1, modifier = Modifier.weight(1f))
+                SquareImage(id = R.drawable.s1, modifier = Modifier.weight(1f))
+            }
+        }
+    }
+}
+
+
+@Composable
+fun StaggeredItem4() {
+    Row() {
+        Column(Modifier.weight(2f)) {
+            Row() {
+                SquareImage(id = R.drawable.s1, modifier = Modifier.weight(1f))
+                SquareImage(id = R.drawable.s1, modifier = Modifier.weight(1f))
+
+            }
+            Row() {
+                SquareImage(id = R.drawable.s1, modifier = Modifier.weight(1f))
+                SquareImage(id = R.drawable.s1, modifier = Modifier.weight(1f))
+            }
+        }
+        RectangleImage(id = R.drawable.s1, modifier = Modifier.weight(1f))
+    }
+}
+
+
+@Composable
+fun SquareImage(id : Int, modifier: Modifier) {
+    Image(
+        painter = painterResource(id = id),
+        contentDescription = "",
+        modifier = modifier
+            .aspectRatio(1f)
+            .padding(5.dp),
+        contentScale = ContentScale.Crop
+    )
+}
+
+@Composable
+fun RectangleImage(id : Int, modifier: Modifier) {
+    Image(
+        painter = painterResource(id = id),
+        contentDescription = "",
+        modifier = modifier
+            .aspectRatio(0.5f)
+            .padding(5.dp),
+        contentScale = ContentScale.Crop
+    )
 }
 
 @Composable
@@ -50,6 +155,39 @@ fun SearchAppbar() {
 
 @Composable
 @Preview
-fun InstagramSearchPreview() {
-    InstagramSearch()
+fun StaggeredItemOnePreview() {
+    StaggeredItem1()
+}
+
+@Composable
+@Preview
+fun StaggeredItemTwoPreview() {
+    StaggeredItem2()
+}
+
+@Composable
+@Preview
+fun StaggeredItemThreePreview() {
+    StaggeredItem3()
+}
+
+@Composable
+@Preview
+fun StaggeredItemFourPreview() {
+    StaggeredItem4()
+}
+
+@Composable
+@Preview
+fun RectangleImageViewPreview() {
+    RectangleImage(id = R.drawable.s1, modifier = Modifier)
+
+}
+
+
+@Composable
+@Preview
+fun SearchAppbarPreview() {
+    SearchAppbar()
+
 }
